1b95777a06299d1a8c7e1ff7f18700134332b369
cph <chewph@gmail.com>
2017-06-02 15:40:09 +0800

    Fully completed tutorial 29 - Http Data Error Handling

--- src/app/http/employee-http.service.ts
+++ src/app/http/employee-http.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class EmployeeHttpService{
 
-	private _url: string = "assets/apidata/employeedata.json";
+	private _url: string = "assets/apidata/employeedata1.json";
 
 	constructor(private _http: Http){
 
 	}
 	getEmployees(){
 		return this._http.get(this._url)
-			.map((response:Response) => response.json());
+			.map((response:Response) => response.json())
+			.catch(this._errorHandler);
 			//		x					y
 			/* x ==> Response object variable input received from http get
 			   y ==> convert response to JSON
 			* */
+	}
+	_errorHandler(error: Response){
+		console.error(error);
+		return Observable.throw(error || "Server Error");
 	}
 }
--- src/app/http/employee-list-http.component.ts
+++ src/app/http/employee-list-http.component.ts
@@ -5,6 +5,7 @@ import { EmployeeHttpService } from "./employee-http.service";
 	selector: 'employee-http-list',
 	template: `
 			<h3>Employee Http List</h3>
+			<h3>{{ errorMsg }}</h3>
 			<ul *ngFor="let employee of employees">
 				<li>{{ employee.name }}</li>				
 			</ul>,
@@ -14,18 +15,22 @@ import { EmployeeHttpService } from "./employee-http.service";
 
 export class EmployeeListHttpComponent implements OnInit{
 	employees = [];
+	errorMsg: string;
 
 	constructor(private _employeeHttpService: EmployeeHttpService){
 	}
 
 	ngOnInit(){
 		this._employeeHttpService.getEmployees()
-			.subscribe(resEmployeeData => this.employees = resEmployeeData);
-			// 				x			y				z
-			/* x ==> Typeless input parameter
-			*  y ==> employees array
-			*  z ==> Assign x to employees array
-			*  */
+			.subscribe(resEmployeeData => this.employees = resEmployeeData,
+				// 				x			y				z
+				/* x ==> Typeless input parameter
+				 *  y ==> employees array
+				 *  z ==> Assign x to employees array
+				 *  */
+					 resEmployeeError => this.errorMsg = resEmployeeError);
+					// second parameter call back function if error occurs
+
 	}
 
 }
