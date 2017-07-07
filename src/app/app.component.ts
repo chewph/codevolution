700dfd915ada356efb0c140b75eee75869183b9e
cph <chewph@gmail.com>
2017-07-07 17:00:55 +0800

    Added named router-outlet sample code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: [ './app.component.css' ]
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
 	title = 'Codevolution Examples';
+
+	constructor(private route: ActivatedRoute, private router: Router){
+	}
+
+
+	ngOnInit(){
+	}
+
+	goContent(){
+
+	}
+
+	goMainPage(){
+		this.router.navigateByUrl('/mainPage/(topMenu-outlet:topMenuPath//content-outlet:contentPath)');
+		//router.navigateByUrl('/articles/(route1:article1//route2:article2)');
+	}
 }
